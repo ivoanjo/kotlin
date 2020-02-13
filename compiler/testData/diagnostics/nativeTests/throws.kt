d63ea20f06e668e29fc5f154b949fbd9e50d6696
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2020-02-13 11:46:59 +0300

    Allow Native `@Throws` on override when the same list is specified

--- compiler/testData/diagnostics/nativeTests/throws.kt
+++ compiler/testData/diagnostics/nativeTests/throws.kt
@@ -11,16 +11,36 @@ class Exception3 : Throwable()
 <!THROWS_LIST_EMPTY!>@Throws<!>
 fun foo() {}
 
+interface Base0 {
+    fun foo()
+}
+
+class ThrowsOnOverride : Base0 {
+    <!INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception1::class)<!> override fun foo() {}
+}
+
 interface Base1 {
     @Throws(Exception1::class) fun foo()
 }
 
-class HasThrowsOnOverride : Base1 {
-    <!THROWS_ON_OVERRIDE!>@Throws(Exception1::class)<!> override fun foo() {}
+class InheritsThrowsAndNoThrows : Base0, Base1 {
+    <!INCOMPATIBLE_THROWS_INHERITED!>override fun foo() {}<!>
+}
+
+class OverridesThrowsAndNoThrows : Base0, Base1 {
+    <!INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception1::class)<!> override fun foo() {}
+}
+
+class SameThrowsOnOverride : Base1 {
+    @Throws(Exception1::class) override fun foo() {}
+}
+
+class DifferentThrowsOnOverride : Base1 {
+    <!INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception2::class)<!> override fun foo() {}
 }
 
 class HasThrowsWithEmptyListOnOverride : Base1 {
-    <!THROWS_ON_OVERRIDE!>@Throws<!> override fun foo() {}
+    <!INCOMPATIBLE_THROWS_OVERRIDE!>@Throws<!> override fun foo() {}
 }
 
 interface Base2 {
@@ -31,8 +51,20 @@ open class InheritsDifferentThrows1 : Base1, Base2 {
     <!INCOMPATIBLE_THROWS_INHERITED!>override fun foo() {}<!>
 }
 
+open class OverridesDifferentThrows1 : Base1, Base2 {
+    <!INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception1::class)<!>override fun foo() {}
+}
+
 class InheritsDifferentThrowsThroughSameClass1 : InheritsDifferentThrows1() {
     override fun foo() {}
+}
+
+class OverridesDifferentThrowsThroughSameClass1 : InheritsDifferentThrows1() {
+    @Throws(Exception1::class) override fun foo() {}
+}
+
+class OverridesDifferentThrowsThroughSameClass2 : InheritsDifferentThrows1() {
+    @Throws(Exception2::class) override fun foo() {}
 }
 
 interface Base3 {
@@ -43,12 +75,48 @@ class InheritsDifferentThrows2 : InheritsDifferentThrows1(), Base3 {
     <!INCOMPATIBLE_THROWS_INHERITED!>override fun foo() {}<!>
 }
 
+class OverridesDifferentThrows2 : InheritsDifferentThrows1(), Base3 {
+    <!INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception3::class)<!>override fun foo() {}
+}
+
+open class OverridesDifferentThrows3 : Base1, Base2 {
+    <!INCOMPATIBLE_THROWS_OVERRIDE, INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception3::class)<!>override fun foo() {}
+}
+
+class InheritsDifferentThrows3 : OverridesDifferentThrows3() {
+    override fun foo() {}
+}
+
+class OverrideDifferentThrows4 : OverridesDifferentThrows3() {
+    override fun foo() {}
+}
+
+class OverrideDifferentThrows5 : OverridesDifferentThrows3() {
+    @Throws(Exception3::class) override fun foo() {}
+}
+
+class OverrideDifferentThrows6 : OverridesDifferentThrows3() {
+    <!INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception1::class)<!> override fun foo() {}
+}
+
 interface Base4 {
     @Throws(Exception1::class) fun foo()
 }
 
 class InheritsSameThrows : Base1, Base4 {
     override fun foo() {}
+}
+
+class OverridesSameThrows : Base1, Base4 {
+    @Throws(Exception1::class) override fun foo() {}
+}
+
+class OverrideDifferentThrows7 : Base1, Base4 {
+    <!INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception2::class)<!> override fun foo() {}
+}
+
+class OverrideDifferentThrows8 : Base1, Base3 {
+    <!INCOMPATIBLE_THROWS_OVERRIDE, INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception2::class)<!> override fun foo() {}
 }
 
 interface Base5 {
@@ -63,6 +131,10 @@ class InheritsSameThrowsMultiple : Base5, Base6 {
     override fun foo() {}
 }
 
+class OverridesSameThrowsMultiple : Base5, Base6 {
+    @Throws(Exception2::class, Exception1::class) override fun foo() {}
+}
+
 fun withLocalClass() {
     class LocalException : Throwable()
 
@@ -72,5 +144,9 @@ fun withLocalClass() {
 
     class InheritsDifferentThrowsLocal : Base1, Base7() {
         <!INCOMPATIBLE_THROWS_INHERITED!>override fun foo() {}<!>
+    }
+
+    class OverridesDifferentThrowsLocal : Base1, Base7() {
+        <!INCOMPATIBLE_THROWS_OVERRIDE!>@Throws(Exception1::class, LocalException::class)<!>override fun foo() {}
     }
 }
--- native/frontend/src/org/jetbrains/kotlin/resolve/konan/diagnostics/NativeThrowsChecker.kt
+++ native/frontend/src/org/jetbrains/kotlin/resolve/konan/diagnostics/NativeThrowsChecker.kt
@@ -6,36 +6,68 @@
 package org.jetbrains.kotlin.resolve.konan.diagnostics
 
 
-import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
+import org.jetbrains.kotlin.descriptors.CallableDescriptor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.annotations.AnnotationDescriptor
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.resolve.DescriptorToSourceUtils
 import org.jetbrains.kotlin.resolve.checkers.DeclarationChecker
 import org.jetbrains.kotlin.resolve.checkers.DeclarationCheckerContext
 import org.jetbrains.kotlin.resolve.constants.ArrayValue
 import org.jetbrains.kotlin.resolve.constants.ConstantValue
 import org.jetbrains.kotlin.resolve.descriptorUtil.firstArgument
-import org.jetbrains.kotlin.resolve.findOriginalTopMostOverriddenDescriptors
+import org.jetbrains.kotlin.utils.DFS
 
 object NativeThrowsChecker : DeclarationChecker {
     private val throwsFqName = FqName("kotlin.native.Throws")
 
     override fun check(declaration: KtDeclaration, descriptor: DeclarationDescriptor, context: DeclarationCheckerContext) {
-        checkForIncompatibleMultipleInheritance(declaration, descriptor, context)
-
-        val throwsAnnotation = descriptor.annotations.findAnnotation(throwsFqName) ?: return
-        val element = DescriptorToSourceUtils.getSourceFromAnnotation(throwsAnnotation) ?: declaration
-
-        if (descriptor is CallableMemberDescriptor && descriptor.overriddenDescriptors.isNotEmpty()) {
-            context.trace.report(ErrorsNative.THROWS_ON_OVERRIDE.on(element))
+        val throwsAnnotation = descriptor.annotations.findAnnotation(throwsFqName)
+        if (throwsAnnotation == null) {
+            checkForIncompatibleMultipleInheritance(declaration, descriptor, context)
             return
         }
 
+        val reportLocation = DescriptorToSourceUtils.getSourceFromAnnotation(throwsAnnotation) ?: declaration
+
+        if (!checkThrowsOnOverride(descriptor, context, throwsAnnotation, reportLocation)) return
+
         if (throwsAnnotation.getVariadicArguments().isEmpty()) {
-            context.trace.report(ErrorsNative.THROWS_LIST_EMPTY.on(element))
+            context.trace.report(ErrorsNative.THROWS_LIST_EMPTY.on(reportLocation))
         }
+    }
+
+    private fun checkThrowsOnOverride(
+        descriptor: DeclarationDescriptor,
+        context: DeclarationCheckerContext,
+        throwsAnnotation: AnnotationDescriptor,
+        reportLocation: KtElement
+    ): Boolean {
+        if (descriptor !is CallableDescriptor || descriptor.overriddenDescriptors.isEmpty()) {
+            // Not an override.
+            return true
+        }
+
+        val reported = mutableSetOf<ThrowsFilter>()
+        val ownThrows = decodeThrowsFilter(throwsAnnotation)
+
+        for (overridden in descriptor.overriddenDescriptors) {
+            val inheritedThrows = findInheritedThrows(overridden)
+            if (ownThrows !in inheritedThrows.values) {
+                // Own @Throws differs from all the inherited ones. Report any:
+                val (member, throws) = inheritedThrows.entries.firstOrNull()
+                    ?: continue // Should not happen though.
+
+                if (throws !in reported) {
+                    reported.add(throws)
+                    context.trace.report(ErrorsNative.INCOMPATIBLE_THROWS_OVERRIDE.on(reportLocation, member.containingDeclaration))
+                }
+            }
+        }
+
+        return reported.isEmpty()
     }
 
     private fun checkForIncompatibleMultipleInheritance(
@@ -43,21 +75,50 @@ object NativeThrowsChecker : DeclarationChecker {
         descriptor: DeclarationDescriptor,
         context: DeclarationCheckerContext
     ) {
-        if (descriptor !is CallableMemberDescriptor) return
+        if (descriptor !is CallableDescriptor) return
 
         if (descriptor.overriddenDescriptors.size < 2) return // No multiple inheritance here.
 
-        val incompatible = descriptor.findOriginalTopMostOverriddenDescriptors()
-            .distinctBy { it.annotations.findAnnotation(throwsFqName)?.getVariadicArguments()?.toSet() }
+        val incompatible = findInheritedThrows(descriptor).entries.distinctBy { it.value }.map { it.key }
 
         if (incompatible.size < 2) return
 
         context.trace.report(ErrorsNative.INCOMPATIBLE_THROWS_INHERITED.on(declaration, incompatible.map { it.containingDeclaration }))
     }
 
+    private fun findInheritedThrows(descriptor: CallableDescriptor): Map<CallableDescriptor, ThrowsFilter> {
+        val result = mutableMapOf<CallableDescriptor, ThrowsFilter>()
+
+        DFS.dfs(
+            listOf(descriptor),
+            { current -> current.overriddenDescriptors },
+            object : DFS.AbstractNodeHandler<CallableDescriptor, Unit>() {
+                override fun beforeChildren(current: CallableDescriptor): Boolean {
+                    val throwsAnnotation = current.annotations.findAnnotation(throwsFqName)
+                    return if (throwsAnnotation == null && !current.overriddenDescriptors.isEmpty()) {
+                        // Visit overridden members:
+                        true
+                    } else {
+                        // Take current and ignore overridden:
+                        result[current.original] = decodeThrowsFilter(throwsAnnotation)
+                        false
+                    }
+                }
+
+                override fun result() {}
+            })
+
+        return result
+    }
+
     private fun AnnotationDescriptor.getVariadicArguments(): List<ConstantValue<*>> {
         val argument = this.firstArgument() as? ArrayValue ?: return emptyList()
         return argument.value
     }
+
+    private fun decodeThrowsFilter(throwsAnnotation: AnnotationDescriptor?) =
+        ThrowsFilter(throwsAnnotation?.getVariadicArguments()?.toSet())
+
+    private data class ThrowsFilter(val classes: Set<ConstantValue<*>>?)
 
 }
