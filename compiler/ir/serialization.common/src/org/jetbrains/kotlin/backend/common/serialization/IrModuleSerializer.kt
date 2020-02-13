d0faefb1fccfdc51da7a1b157865ba52aac49ca8
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-02-13 16:40:57 +0700

    [IR][Serialization] Allow backends to skip files
    This change is required for Native interop libraries.
    For enums and structs we generate full-blown IR classes that should not
    be serialized because they will be generated anew.

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/IrModuleSerializer.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/IrModuleSerializer.kt
@@ -15,12 +15,23 @@ import org.jetbrains.kotlin.library.SerializedIrModule
 abstract class IrModuleSerializer<F : IrFileSerializer>(protected val logger: LoggingContext) {
     abstract fun createSerializerForFile(file: IrFile): F
 
+    /**
+     * Some files may contain declarations that should not be serialized.
+     * For example, it might be classes that should be generated instead of deserialization.
+     */
+    protected open fun backendSpecificFileFilter(file: IrFile): Boolean =
+        true
+
     private fun serializeIrFile(file: IrFile): SerializedIrFile {
         val fileSerializer = createSerializerForFile(file)
         return fileSerializer.serializeIrFile(file)
     }
 
     fun serializedIrModule(module: IrModuleFragment): SerializedIrModule {
-        return SerializedIrModule(module.files.filter { it.packageFragmentDescriptor !is FunctionInterfacePackageFragment }.map { serializeIrFile(it) })
+        val serializedFiles = module.files
+            .filter { it.packageFragmentDescriptor !is FunctionInterfacePackageFragment }
+            .filter(this::backendSpecificFileFilter)
+            .map(this::serializeIrFile)
+        return SerializedIrModule(serializedFiles)
     }
 }
